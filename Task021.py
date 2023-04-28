9cecccc9f46d1379e28572d451abc827106048a1
theos <theossoft@gmail.com>
2023-04-28 18:39:59 +0300

    Задача 21 решена.

---
+++ Task021.py
@@ -0,0 +1,17 @@
+# Задача №21. Решение в группах
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+# {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+# ":" S007 "}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+# Примечание: Список словарей задан изначально.
+# Пользователь его не вводит
+
+some_list = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
+output_set = set()
+for i in range(0, len(some_list)):
+    output_set.add(list(some_list[i].values()))
+# for i in some_list:
+#     output_set.add(list(i.values())[0].strip())
+print(output_set)
