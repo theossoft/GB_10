9c248565f2a5c3adb544592c10d744022549f082
theos <theossoft@gmail.com>
2023-05-11 23:52:54 +0300

    Задача 39. Добавлено второе решение.

--- Task039.py
+++ Task039.py
@@ -14,6 +14,7 @@
 # 3 3 2 12
 # (каждое число вводится с новой строки)
 
+# Мой вариант
 arr1 = [3, 1, 3, 4, 2, 4, 12]
 arr2 = [4, 15, 43, 1, 15, 1]
 string = str()
@@ -25,5 +26,15 @@ for i in range(0, len(arr1)):
         if arr1[i] != arr2[-1]:
             string = string + " " + str(arr1[i])
             break
-
 print(string)
+
+# Не мой вариант
+use_list1 = int(input("Введите размер первого массива: "))
+list_one = [int(input(f"{i+1} number = ")) for i in range (use_list1)]
+use_list2 = int(input("Введите размер второго массива: "))
+list_two = [int(input(f"{i+1} number = ")) for i in range(use_list2)]
+print(*list_one)
+list_two = set(list_two)
+print(*list_two)
+temp_list = [i for i in list_one if not i in list_two]
+print(*temp_list)
